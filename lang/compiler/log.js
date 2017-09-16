7f81dde4f8e633a4e211f6525beb181b2d5b5a5e
Callum Hart <callum@callumhart.com>
2017-09-16 19:36:40 +0200

    [lang] parsing mono files begun

--- lang/compiler/log.js
+++ lang/compiler/log.js
@@ -1,36 +1,36 @@
 module.exports = {
   cli: {
     NO_ARGUMENT: () => {
-      console.log('No argument(s) present');
+      console.log('[Cli] No argument(s) present');
       process.exit();
     },
     UNRECOGNIZED_ARGUMENT: (arg) => {
-      console.log(`Unrecognized argument: ${arg}`);
+      console.log(`[Cli] Unrecognized argument: ${arg}`);
       process.exit();
     }
   },
   config: {
     CONFIG_ERROR: (err) => {
-      console.log(`Error with config \n => ${err}`);
+      console.log(`[Config] Error with config \n => ${err}`);
       process.exit();
     },
     CONFIG_VALIDATION_ERRORS: (fileName, errors) => {
-      console.log(`Validation of ${fileName} failed`);
+      console.log(`[Config] Validation of ${fileName} failed`);
       errors.forEach((err) => console.log(` => ${err}`));
       process.exit();
     },
     CONFIG_SAMPLE_CREATED: (fileName, location) => {
-      console.log(`Added ${fileName} to project \n => Located: ${location}`);
+      console.log(`[Config] Added ${fileName} to project \n => Located: ${location}`);
       process.exit();
     },
     CONFIG_SAMPLE_FAILED: (fileName, err) => {
-      console.log(`Failed to create ${fileName} \n => ${err}`);
+      console.log(`[Config] Failed to create ${fileName} \n => ${err}`);
       process.exit();
     }
   },
   parser: {
     SOURCE_ERROR: (err) => {
-      console.log(`Error with mono source file \n => ${err} `);
+      console.log(`[Parser] Error with mono source file \n => ${err} `);
       process.exit();
     }
   }
--- lang/compiler/parser.js
+++ lang/compiler/parser.js
@@ -1,24 +1,25 @@
 const fs = require('fs');
 const _ = require('lodash');
+const chalk = require('chalk');
 
 
 const Log = require('./log').parser;
 const Config = require('./config');
 const EXTENSION = '.mono';
 let ast = {
-  source: [],
+  files: [],
 }
 
 const run = () => {
   try {
     Config.getSrc().forEach((file, order) => {
-      const fileName = _.endsWith(file, EXTENSION) ? file : `${file}${EXTENSION}`;
-      const monoSource = fs.readFileSync(`${process.cwd()}/${fileName}`).toString();
+      const name = _.endsWith(file, EXTENSION) ? file : `${file}${EXTENSION}`;
+      const source = fs.readFileSync(`${process.cwd()}/${name}`).toString();
 
-      ast.source.push({
-        fileName,
+      ast.files.push({
+        name,
         order,
-        monoSource
+        source
       });
     });
 
@@ -29,7 +30,32 @@ const run = () => {
 }
 
 const parse = () => {
-  console.log(ast.source);
+  ast.files.forEach(file => parseFile(file));
+}
+
+const parseFile = (file) => {
+  /*
+  todo: parse each file, adding to ast:
+  - rule-sets
+      selector
+      styles
+      order (in file)
+  - media queries
+    - breakpoints
+    - order (in file)
+    - rule-sets
+  */
+
+  console.log(chalk.blue.bold(`\nFile: ${file.name} --------------- \n`));
+  console.log(chalk.gray(`${file.source}\n`));
+
+  console.log(chalk.blue(`Selectors (in ${file.name}) : --------------- \n`));
+  const selectors = file.source
+                      .replace(/ {/g, '{')   // remove space before opening brace
+                      .replace(/,\n/g, ', ') // put multiple selectors on single line
+                      .match(/^.*(?={)/gm);  // anything before opening brace
+
+  console.log(selectors);
 }
 
 module.exports = {
