a8ad2186bd49489c1a05dcc822e62b0fe380a857
Callum Hart <callum@callumhart.com>
2017-10-01 12:48:15 +0200

    [lang] lexer - @charset and CSS declaration tokens

--- lang/compiler/lexer.js
+++ lang/compiler/lexer.js
@@ -31,8 +31,9 @@ const ESCAPED_SELECTOR        = 'ESCAPED_SELECTOR';  // button.\*btn
 const BRACE_OPEN              = 'BRACE_OPEN';        // {
 const BRACE_CLOSE             = 'BRACE_CLOSE';       // }
 const MEDIA_QUERY             = 'MEDIA_QUERY';       // @media (min-width: 300px) and (max-width: 600px)
-const FONT_FACE               = 'FONT_FACE';         // @font-face
 const KEYFRAME                = 'KEYFRAME';          // @keyframes
+const FONT_FACE               = 'FONT_FACE';         // @font-face
+const CHARSET                 = 'CHARSET';           // @charset
 const COMMENT_OPEN            = 'COMMENT_OPEN';      // /*
 const COMMENT_CLOSE           = 'COMMENT_CLOSE';     // */
 const INLINE_COMMENT          = 'INLINE_COMMENT';    // /* an inline comment */
@@ -110,6 +111,19 @@ const getToken = (value, pos) => {
   if (isFontFace(value)) {
     return atRule(pos, FONT_FACE, value);
   }
+
+  if (isCharSet(value)) {
+    return atRule(pos, CHARSET, value);
+  }
+
+  if (isDeclaration(value)) {
+    return declaration(pos, value);
+  }
+
+  // todo: looks like formatter is appending empty line to each file.
+  if (value === '') {
+    console.log('EMPTY VALUE');
+  }
 }
 
 // Token identifiers --
@@ -147,6 +161,16 @@ const isKeyFrame = value => {
 const isFontFace = value => {
   // line is a @font-face at-rule i.e: `@font-face {`
   return value.match(/^@font-face.*{$/);
+}
+
+const isCharSet = value => {
+  // line is a @charset at-rule i.e: `@charset "UTF-8";`
+  return value.match(/^@charset.*;$/);
+}
+
+const isDeclaration = value => {
+  // line is a CSS declaration i.e: `background-color: rgba(0,0,0,.1);`
+  return value.match(/[a-zA-Z]\s*[^\n]+\s*;/);
 }
 
 
@@ -193,45 +217,71 @@ const inlineComment = (pos, comment) => {
 }
 
 /**
- * Get tokens for a given at-rule. Can be:
+ * Get token(s) for a given at-rule. Can be:
+ *
  * - `@media`
  * - `@keyframe`
  * - `@font-face`
+ * - `@charset`
  *
- * An at-rule consists of 2 tokens:
+ * An at-rule can consist of 1 or 2 tokens, depending on its type:
  *
- * 1. the first being the rule i.e: `@keyframes bounce`
- * 2. the second being its opening brace i.e: `{`
+ * 1. first token is always the rule i.e: `@keyframes bounce`
+ * 2. second [optional] token is an opening brace (if at-rule has one) i.e: `{`
  *
  * @param  {Number} pos   - index of current line in file
  * @param  {String} type  - type of at-rule
- * @param  {String} value - fully qualified at-rule (including its open brace)
- * @return {Array}        - 2 tokens
+ * @param  {String} value - fully qualified at-rule
+ * @return {Array}        - array of 1 or 2 tokens
  */
 const atRule = (pos, type, value) => {
   const tokens = new Array();
-  tokens.plural = true;
+  const line = pos + 1;
 
-  tokens.push(
-    [
-      type,
-      value.replace(/{/, ''),
-      {
-        line: pos + 1
-      }
-    ],
-    [
-      BRACE_OPEN,
-      '{',
-      {
-        line: pos + 1,
-        col: value.indexOf('{') + 1
-      }
-    ]
-  );
+  switch (type) {
+    case CHARSET:
+      tokens.push(
+        CHARSET,
+        value,
+        {
+          line
+        }
+      );
+      break;
+    default:
+      tokens.plural = true;
+
+      tokens.push(
+        [
+          type,
+          value.replace(/{/, ''),
+          {
+            line
+          }
+        ],
+        [
+          BRACE_OPEN,
+          '{',
+          {
+            line,
+            col: value.indexOf('{') + 1
+          }
+        ]
+      );
+      break;
+  }
 
   return tokens;
 }
 
+const declaration = (pos, declaration) => {
+  return [
+    DECLARATION,
+    declaration,
+    {
+      line: pos + 1
+    }
+  ];
+}
 
 module.exports = { tokenize }
