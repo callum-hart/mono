04feceadcd14c4a6b34ee765c9f8ba2b980a6d40
Callum Hart <callum@callumhart.com>
2017-12-20 15:22:02 +0100

    [csx] detect overrides among equal refs

--- misc/jsx-like-syntax/mvp/index.js
+++ misc/jsx-like-syntax/mvp/index.js
@@ -3,8 +3,11 @@
  */
 
 
+const BLANK = "";
 const SPACE = " ";
 const DOT = ".";
+const COLON = ":";
+const SEMI_COLON = ";"
 
 
 const Mono = {
@@ -17,41 +20,88 @@ const Mono = {
     };
   },
 
-  creatCSS(a) {
-    a.forEach(b => Mono._makeCSS(b));
+  createCSS(a) {
+    a.forEach(b => Mono._parseCSS(b));
     Mono._parseAst();
   },
 
-  _makeCSS(s, parentRef = null) {
-    const ref = Mono._makeRef(s);
-    const fullRef = parentRef ? `${parentRef}${SPACE}${ref}` : ref;
+  _parseCSS(c, parentRef = null) {
+    const ref = Mono._makeRef(c);
+    const fullyQualifiedRef = parentRef ? `${parentRef}${SPACE}${ref}` : ref;
+    Mono._saveRef(fullyQualifiedRef, c.styles);
 
-    console.log(`fullRef: ${fullRef}`);
-    Mono._saveRef(fullRef, s.styles);
-
-    if (s.children.length) {
+    if (c.children.length) {
       if (parentRef) {
         parentRef += `${SPACE}${ref}`;
       } else {
         parentRef = ref;
       }
 
-      s.children.map(b => Mono._makeCSS(b, parentRef));
+      c.children.forEach(d => Mono._parseCSS(d, parentRef));
     }
   },
 
   _parseAst() {
-    console.log(Mono._AST);
+    for (var ref in Mono._AST) {
+      console.log(ref);
+      console.log(Mono._AST[ref]);
+
+      ref.split(SPACE)
+          .map(b => b.split(DOT))
+          .forEach(c => {
+            console.log(c);
+          });
+
+      console.log('------------------------');
+    }
   },
 
   _AST: {},
 
   _saveRef(ref, styles) {
     if (Mono._AST[ref]) {
-      console.log(`ref: ${ref} already saved, merge styles with existing ref?`);
+      try {
+        Mono._stylesClash(Mono._AST[ref], styles);
+        // merge styles
+        Mono._AST[ref] += styles;
+      } catch (e) {
+        throw new e.constructor(e.message);
+      }
     } else {
       Mono._AST[ref] = styles;
     }
+  },
+
+  _stylesClash(existingStyles, newStyles) {
+    for (var property in Mono._stylesAsObject(newStyles)) {
+      if (Mono._stylesAsObject(existingStyles)[property]) {
+        throw new Error(`
+          The property: '${property}' has already been defined.
+
+          existingStyles:
+            ${existingStyles}
+
+          newStyles:
+            ${newStyles}
+        `);
+      }
+    }
+
+    return false;
+  },
+
+  _stylesAsObject(styles) {
+    const styleObj = {}
+
+    styles.split(SEMI_COLON)
+          .filter(res => res !== BLANK)
+          .map(res => res.trim())
+          .forEach(declaration => {
+            const [property, value] = declaration.split(COLON).map(res => res.trim());
+            styleObj[property] = value;
+          });
+
+    return styleObj;
   },
 
   _makeSelector({element, className}) {
@@ -82,6 +132,18 @@ const styles = [
     "form",
     null,
     "display: block;"
+  ),
+
+  Mono.createStyle(
+    "span",
+    null,
+    "color: blue; font-style: italic;"
+  ),
+
+  Mono.createStyle(
+    "span",
+    null,
+    "font-size: 23px; font-weight: bold; font-style:italic;line-height: 20px;"
   ),
 
   Mono.createStyle(
@@ -139,4 +201,4 @@ const styles = [
 ];
 
 
-Mono.creatCSS(styles);
+Mono.createCSS(styles);
